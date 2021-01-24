e249216077832b64ce166397e29ce29f2247e431
mounika216 <cheera.mounika@gmail.com>
2021-01-24 10:15:25 +0530

    Special services are available here

---
+++ RegularTicket.java
@@ -0,0 +1,27 @@
+public class RegularTicket extends  Ticket{
+
+    private String specialService;
+
+    RegularTicket(String departureLocation,
+            String destinationLocation,
+            Flight flight,
+            String dateTimeOfDeparture,
+            String dateTimeOfArrival,
+            Passenger passenger,
+            int seatNumber,
+            double costPrice,
+            boolean statusCancelled,
+            String specialService) {
+        super(departureLocation,destinationLocation,flight,dateTimeOfDeparture,dateTimeOfArrival,
+        passenger,seatNumber,costPrice,statusCancelled);
+        this.specialService = specialService;
+    }
+
+    public String getSpecialService() {
+        return this.specialService;
+    }
+    public void updateSpecialService(String specialService) {
+        this.specialService = specialService;
+    }
+
+}
